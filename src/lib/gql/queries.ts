3a164df34b849058c82fec223eadbcb97b2b61e1
CryptoGraffe <graffe@graffe.co>
2022-04-09 15:23:29 -0700

    fixes #759 - porting getProfile query to graphql

--- src/lib/gql/queries.ts
+++ src/lib/gql/queries.ts
@@ -1,3 +1,5 @@
+import { IApiProfile } from '../../types';
+
 import { client } from './client';
 
 export const getCurrentEpoch = async (
@@ -36,4 +38,65 @@ export const getDiscordWebhook = async (circleId: number) => {
     ],
   });
   return circle_private.pop()?.discord_webhook;
+};
+
+export const getProfile = async (address: string): Promise<IApiProfile> => {
+  const { profiles } = await client.query({
+    profiles: [
+      {
+        where: {
+          address: { _ilike: address },
+        },
+      },
+      {
+        id: true,
+        address: true,
+        admin_view: true,
+        avatar: true,
+        background: true,
+        bio: true,
+        discord_username: true,
+        github_username: true,
+        medium_username: true,
+        telegram_username: true,
+        twitter_username: true,
+        website: true,
+        skills: true,
+        created_at: true,
+        updated_at: true,
+        users: [
+          {},
+          {
+            id: true,
+            circle_id: true,
+            name: true,
+            address: true,
+            non_giver: true,
+            fixed_non_receiver: true,
+            bio: true,
+            starting_tokens: true,
+            non_receiver: true,
+            give_token_received: true,
+            created_at: true,
+            updated_at: true,
+            give_token_remaining: true,
+            role: true,
+            epoch_first_visit: true,
+          },
+        ],
+      },
+    ],
+  });
+
+  const p = profiles.pop();
+  if (!p) {
+    throw 'unable to load address: ' + address;
+  }
+
+  const adaptedProfile: Omit<typeof p, 'skills'> & { skills?: string[] } = {
+    ...p,
+    skills: p.skills && JSON.parse(p.skills),
+  };
+
+  return adaptedProfile;
 };
